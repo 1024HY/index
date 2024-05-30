3bda05b39f879e6815dd1bf9c30e41826fb9cf4a
1024HY <2727968152@qq.com>
2024-05-30 15:29:30 +0800

    测试

---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="navbar">
+    <div class="navbar-left">
+      <!-- 左侧内容 -->
+      <slot name="left" />
+    </div>
+    <div class="navbar-title">
+      <!-- 标题 -->
+      {{ props.title }}
+    </div>
+    <div class="navbar-right">
+      <!-- 右侧内容 -->
+      <slot name="right" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps({
+  title: String // 标题
+});
+</script>
+
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 60px; /* 导航栏高度 */
+  background-color: #0f62c4; /* 导航栏背景色 */
+  color: #ffffff; /* 文字颜色 */
+  padding: 0 20px; /* 左右内边距 */
+}
+
+.navbar-left,
+.navbar-right {
+  display: flex;
+  align-items: center;
+}
+
+.navbar-title {
+  font-size: 20px;
+}
+</style>
